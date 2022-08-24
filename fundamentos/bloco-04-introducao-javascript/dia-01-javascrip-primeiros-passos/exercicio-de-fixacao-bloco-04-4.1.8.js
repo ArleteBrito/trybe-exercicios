c060487942b74e4a214f61fa7ce3a4e801059af0
Arlete Brito <arletebrittto@gmail.com>
2022-08-24 00:00:27 -0300

    exercicio-de-fixacao-bloco-04-4.1.8.js

---
+++ fundamentos/bloco-04-introducao-javascript/dia-01-javascrip-primeiros-passos/exercicio-de-fixacao-bloco-04-4.1.8.js
@@ -0,0 +1,8 @@
+let weekDay = "quarta-feira";
+
+if (weekDay === "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira" || weekDay === "sexta-feira") {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+}
+else if (weekDay === "sabado " || weekDay === "domingo") {
+    console.log("FINALMENTE, descanso merecido UwU");
+}
