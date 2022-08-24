17a6f5de147bb1876b13de97bde5038534c9298d
Arlete Brito <arletebrittto@gmail.com>
2022-08-24 00:44:31 -0300

    exercicio-de-fixacao-bloco-04-4.1.12.js

---
+++ fundamentos/bloco-04-introducao-javascript/dia-01-javascrip-primeiros-passos/exercicio-de-fixacao-bloco-04-4.1.12.js
@@ -0,0 +1,10 @@
+const maiorNumero = 50;
+const menorNumero = 100;
+
+if (maiorNumero > menorNumero) {
+    console.log("Maior número é maior que menor número");
+
+} else {
+    console.log("Menor número é maior que maior número");
+}
+
