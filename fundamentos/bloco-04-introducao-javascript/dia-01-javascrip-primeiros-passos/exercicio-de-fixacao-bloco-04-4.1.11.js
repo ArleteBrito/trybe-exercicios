a2a029a23d556f0457fd13f84ba3185b8ddb77ca
Arlete Brito <arletebrittto@gmail.com>
2022-08-24 00:34:24 -0300

    exercicio-de-fixacao-bloco-04-4.1.11.js

---
+++ fundamentos/bloco-04-introducao-javascript/dia-01-javascrip-primeiros-passos/exercicio-de-fixacao-bloco-04-4.1.11.js
@@ -0,0 +1,8 @@
+const mesada = 100;
+const salario = 500;
+
+console.log(mesada + salario);
+console.log(mesada - salario);
+console.log(mesada * salario);
+console.log(mesada / salario);
+console.log(mesada % salario);
