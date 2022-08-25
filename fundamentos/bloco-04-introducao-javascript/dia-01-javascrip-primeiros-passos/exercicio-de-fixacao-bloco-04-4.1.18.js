319246967156ab56084ec2778d37f95192ae5c6f
Arlete Brito <arletebrittto@gmail.com>
2022-08-25 16:15:47 -0300

    exercicio-de-fixacao-bloco-04-4.1.18.js-atualizacao

--- fundamentos/bloco-04-introducao-javascript/dia-01-javascrip-primeiros-passos/exercicio-de-fixacao-bloco-04-4.1.18.js
+++ fundamentos/bloco-04-introducao-javascript/dia-01-javascrip-primeiros-passos/exercicio-de-fixacao-bloco-04-4.1.18.js
@@ -2,9 +2,9 @@ const x = 80;
 const y = 40;
 const z = 60;
 
-let numeroImpar= false;
+let numeroImpar = false;
 
-if ((x % 2 === 0 || y % 2 === 0 || z %2 === 0)) {
+if ((x % 2 === 0 || y % 2 === 0 || z % 2 === 0)) {
     numeroImpar = true
 }
 console.log(numeroImpar);
