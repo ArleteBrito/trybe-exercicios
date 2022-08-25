5ba84a7b5df498bb7cd36ed60c93f53179bc236a
Arlete Brito <arletebrittto@gmail.com>
2022-08-25 14:44:30 -0300

    exercicio-de-fixacao-bloco-04-4.1.13.js

---
+++ fundamentos/bloco-04-introducao-javascript/dia-01-javascrip-primeiros-passos/exercicio-de-fixacao-bloco-04-4.1.13.js
@@ -0,0 +1,14 @@
+const x = 50;
+const y = 100;
+const z = 150;
+
+if (x > y && x > z) {
+    console.log("X é o maior número")
+}
+else if (y > x && y > z) {
+    console.log("Y é o maior número")
+}
+
+else {
+    console.log("Z é o maior número")
+}
