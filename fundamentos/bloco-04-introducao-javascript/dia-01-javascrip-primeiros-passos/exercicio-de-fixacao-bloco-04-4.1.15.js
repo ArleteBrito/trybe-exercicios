5dd23ffa8e09d3896298a53acbc50ff0c3a79f52
Arlete Brito <arletebrittto@gmail.com>
2022-08-25 15:03:54 -0300

    exercicio-de-fixacao-bloco-04-4.1.15.js

---
+++ fundamentos/bloco-04-introducao-javascript/dia-01-javascrip-primeiros-passos/exercicio-de-fixacao-bloco-04-4.1.15.js
@@ -0,0 +1,14 @@
+const x = 80;
+const y = 40;
+const z = 60;
+
+if (x + y + z == 180) {
+    console.log("true")
+}
+else if (x + y + z !== 180) {
+    console.log("false")
+}
+
+else {
+    console.log("não é um triângulo")
+}
