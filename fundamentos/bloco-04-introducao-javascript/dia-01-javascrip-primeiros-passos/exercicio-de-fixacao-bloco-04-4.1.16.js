bd2eb2aebd28db9b376b66f657adeb388833aaa7
Arlete Brito <arletebrittto@gmail.com>
2022-08-25 23:51:03 -0300

    exercicio-de-fixacao-bloco-04-4.1.16.js

---
+++ fundamentos/bloco-04-introducao-javascript/dia-01-javascrip-primeiros-passos/exercicio-de-fixacao-bloco-04-4.1.16.js
@@ -0,0 +1,16 @@
+let nome89 = "BISPO";
+
+if (nome89.toLowerCase() === "bispo") {
+    console.log("diagonal");
+}
+else if (nome89.toLowerCase() === "cavalo") {
+    console.log("l");
+}
+
+else if (nome89.toLowerCase() === "torre") {
+    console.log("cruz");
+}
+
+else {
+    console.log("erro");
+}
