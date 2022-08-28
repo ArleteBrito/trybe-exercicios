86c612fc7d7de2dc8a4f57390334055b93576ded
Arlete Brito <arletebrittto@gmail.com>
2022-08-28 14:56:17 -0300

    exercicio-de-fixacao-bloco-04-4.4.6.js

---
+++ fundamentos/bloco-04-introducao-javascript/dia-04-javacsript-objetos-e-funcoes/exercicio-de-fixacao-bloco-04-4.4.6.js
@@ -0,0 +1,11 @@
+// 3 - Adicione ao objeto a chave bestInTheWorld e 
+// atribua a esta chave um array contendo as datas em que a jogadora Marta foi considerada a melhor do mundo.
+
+
+let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+
